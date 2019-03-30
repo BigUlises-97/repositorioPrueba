fc7f7a042830b9f2a9fba96bc283bac2baf648c0
bigulises <bigulises@hotmail.es>
2019-03-29 18:07:54 -0700

    primer commit

---
+++ PruebaJenkins.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajenkins;
+
+/**
+ *
+ * @author bigul
+ */
+public class PruebaJenkins {
+
+    public int suma(int num1, int num2){
+        return num1+num2;
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Prueba para jenkins
+    }
+    
+}
